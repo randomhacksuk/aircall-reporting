ca38c6714b575effb653524d44c36853a56cf946
User <User>
2017-05-09 16:04:53 +0400

    aircall-users id test

--- app/Http/Controllers/AircallController.php
+++ app/Http/Controllers/AircallController.php
@@ -11,9 +11,14 @@ class AircallController extends Controller
 {
     public function postAircallUsers(Request $request)
     {
-    	Log::info($request->all());
-    	// $client = new AircallClient('22e6df7296b0dff9e4080909ae82d604', '7a333e7255576eb86f7479b99643f9db');
-    	// dd($client->users->getUsers());
+    	$data = $request->all();
+    	Log::info($data['id']);
+    	// // $client = new AircallClient('22e6df7296b0dff9e4080909ae82d604', '7a333e7255576eb86f7479b99643f9db');
+    	// // dd($client->users->getUsers());
+    	// if($data['resource'] == 'user' && $data['event'] == 'user.created') {
+
+    	// }
     }
 }
+
  
